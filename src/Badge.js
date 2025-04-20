b466401d3a3eb1936fce2958bb2ae94983f18d4a
Messaadi-Chaima <messaadichaimamanel@gmail.com>
2025-04-20 11:54:25 +0100

    dimensions4

--- src/Badge.js
+++ src/Badge.js
@@ -266,7 +266,7 @@ const Badge = () => {
   };
 
   const downloadBadge = () => {
-    const dataURL = stageRef.current.toDataURL({ pixelRatio: 4 });
+    const dataURL = stageRef.current.toDataURL({ pixelRatio: 2 });
     const link = document.createElement("a");
     link.href = dataURL;
     link.download = "badge.png";
@@ -530,8 +530,8 @@ const Badge = () => {
         </Box>
 
         <Stage
-          width="100%"
-          height="100%"
+          width={1010}
+          height={638}
           onMouseDown={handleDeselect}
           ref={stageRef}
           style={{ border: `2px solid ${primaryColor}`, borderRadius: '10px', backgroundColor: '#fff' }}
