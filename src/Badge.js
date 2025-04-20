8214e672d2d71e4c9571cb886ef7b9113273f674
Messaadi-Chaima <messaadichaimamanel@gmail.com>
2025-04-20 14:55:38 +0100

    dimensions13

--- src/Badge.js
+++ src/Badge.js
@@ -530,8 +530,8 @@ const Badge = () => {
         </Box>
 
         <Stage
-          width={340}
-          height={210}
+          width={844}
+          height={521}
           onMouseDown={handleDeselect}
           ref={stageRef}
           style={{ border: `2px solid ${primaryColor}`, borderRadius: '10px', backgroundColor: '#fff' }}
